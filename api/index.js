2110ae18c46049e91a3dfda86f0848a5efa56c1e
Luiz Saraiva <luizsaraiva50@gmail.com>
2023-10-22 01:15:47 -0300

    Iniciando projeto

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json)
+const PORT = 3000
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({
+        mesnagem: "Conectado"
+    })}
+)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
+
+module.exports = app
