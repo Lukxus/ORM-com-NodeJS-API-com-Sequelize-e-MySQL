c994fa28bfa5014e7eb675ca11c95ed1a60dc6a0
Luiz Saraiva <luizsaraiva50@gmail.com>
2023-10-22 02:23:58 -0300

    Primeiro modulo

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 const PORT = 3000
 
 app.get('/teste', (req, res) => {
